ef90dff6fcd1a183a4fabf9a075a4d9c1ad057ba
shirin <75791599+shirin1996@users.noreply.github.com>
2022-05-02 12:29:27 +0430

    chapter 4-- Q1 added

--- chapter4/tree.py
+++ chapter4/tree.py
@@ -1,1 +1,32 @@
 #Question 1
+class graph:
+    def __init__(self, gdict):
+        if gdict is None:
+            gdict = {}
+        self.gdict = gdict
+
+    def addedge(self,vertex, edge):
+        self.gdict[vertex].append(edge)
+
+
+    def is_route(self, start,end):
+        queue = [start]
+        visited = [start]
+        path = False
+
+        while queue:
+            node_deq = queue.pop(0)
+            for adj in self.gdict[node_deq]:
+                if adj not in visited:
+                    if adj == end:
+                        path == True
+                        break
+                    else:
+                        visited.append(adj)
+                        queue.append(node_deq)
+
+            return path
+        
+
+
+        
